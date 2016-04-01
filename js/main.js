a50234868d6bdc43fd7c77f2eb9126070fde1656
SarahDowling <SarahandMac2@Sarahs-MacBook-Pro.local>
2016-04-01 17:00:27 -0400

    added first shape js in

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$('.btn-show-hide').on('click', function () {
+  $('.box').toggleClass('show-box');
+});
+
+$('.btn-move').on('click', function () {
+  $('.diamond').toggleClass('move-diamond');
+});
+
+$('.btn-collapse-expand').on('click', function () {
+  $('.box').toggleClass('expand');
+});
+
+// btn-collapse-expand
+//listen for button click
+// when clicked
+// change the height
+//
+//
+//
+//
+
+$('.btn-show-hide').on('click', function () {
+  $('.box').toggleClass('show-box');
+});
+// btn-bounce
+//listen for button click
+// when clicked
+// bounce in left animation
+//put back when done
+//
+//
+//
+
+
+// create an li and put in ul, in css li ahve an animation, mx heigh
+
+// btn-append
+// listen for button click
+// when clicked
+// append&expand
